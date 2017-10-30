b1c355e817fdbcca7fada1090f4d740cf34a46fa
Spencer Smith <WankFerret@Stevens-MacBook.local>
2017-10-30 12:29:06 -0500

    donen

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,27 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+var INCREMENT = 'INCREMENT';
+var DECREMENT = 'DECREMENT';
+
+var initialState = {
+    currentValue: 0
+};
+
+export default function counter(state = initialState, action) {
+    switch(action.type){
+        case INCREMENT:
+            return Object.assign({}, state, {currentValue: state.currentValue + action.amount});
+        case DECREMENT:
+            return Object.assign({}, state, {currentValue: state.currentValue - action.amount})
+        default:
+            return state;
+    }
+    
+}
+export function increment(amount){
+    return {amount, type: INCREMENT}
+}
+export function decrement(amount){
+    return {amount, type: DECREMENT}
+}
